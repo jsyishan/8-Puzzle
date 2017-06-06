6d08496fadf17450f8fc6673caed9bd102f06766
jsyishan <404678173@qq.com>
2017-06-06 19:29:48 +0800

    init commit

---
+++ src/Solver.java
@@ -0,0 +1,35 @@
+/**
+ * Created by eugen on 6/6/17.
+ */
+//
+//public class Solver {
+//    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
+//    public boolean isSolvable()            // is the initial board solvable?
+//    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+//    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+//    public static void main(String[] args) // solve a slider puzzle (given below)
+//}
+
+public class Solver {
+
+    public Solver (Board initial) {
+
+    }
+
+    public boolean isSolvable () {
+
+    }
+
+    public int moves () {
+
+    }
+
+    public Iterable<Board> solution () {
+
+    }
+
+    public static void main (String[] args) {
+
+    }
+
+}
